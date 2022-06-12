42a33b6c7f74e4c5ab75bc5602d02838c1e521e9
monanzr <monanazari8@hmail.com>
2022-06-12 06:58:43 +0430

    add responsive menu

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("navSide").style.width = "min(100%, 23rem)";
+}
+
+function closeNav() {
+    document.getElementById("navSide").style.width = "0";
+}
